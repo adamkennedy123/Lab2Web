6117da247acd9033ec3282f5d3376e65c3f74e72
adamkennedy123 <92745982+adamkennedy123@users.noreply.github.com>
2023-03-19 20:34:10 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form 1</title>
+</head>
+
+<body>
+    <form action="php_dasar.php" method="POST">
+        <table align="center" cellspacing="0" cellpadding="5" bordercolor="black">
+            <tr>
+                <th colspan="3"><u>FORM 1 : INPUT DATA</u></th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text" name="nama" placeholder="Nama Anda" /></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>:</td>
+                <td>
+
+                    <input type="radio" name="jenis_kelamin" value="laki-
+laki" required />Laki - Laki
+
+                    <input type="radio" name="jenis_kelamin" value="perempuan" /> Perempuan
+                </td>
+            </tr>
+            <tr>
+                <td>Tanggal lahir</td>
+                <td>:</td>
+                <td>
+                <input type="date" name="tanggal_lahir"><br>
+                </td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>:</td>
+                <td><select name="pekerjaan">
+                        <option selected readonly>Pilih Pekerjaan...</option>
+                        <option value="Programmer">Programmer</option>
+                        <option value="Data Scientist">Data scientist</option>
+                        <option value="Game Developer">Game Developer</option>
+                        <option value="UX Designer">UX Designer</option>
+                    </select></td>
+            </tr>
+            
+            <tr>
+                <td>Asal Kota</td>
+                <td>:</td>
+                <td><select name="asal_kota">
+                        <option selected readonly>Pilih Kota...</option>
+                        <option value="Kota Jakarta">Jakarta</option>
+                        <option value="Kota Bandung">Bandung</option>
+                        <option value="Kota Bekasi">Bekasi</option>
+                    </select></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>
+                    <textarea class="form-control" rows="5" cols="40" id="comment" name="alamat"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <th colspan="3">
+                    <input type="submit" value="Simpan">
+                    <input type="reset" value="Reset">
+                </th>
+            </tr>
+        </table>
+    </form>
+</body>
